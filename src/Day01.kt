cf7ca1338bf5bd58558e0feb078abe9db46b5880
AllePilli <peal@odoo.com>
2024-12-01 10:53:18 +0100

    Solved Part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,38 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list = input.map { line ->
+            val (first, second) = line.split("""\s+""".toRegex()).map(String::toInt)
+            first to second
+        }
+
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+
+        for ((first, second) in list) {
+            list1.add(first)
+            list2.add(second)
+        }
+
+        list1.sort()
+        list2.sort()
+
+        return list1.zip(list2).fold(0) { acc, (first, second) ->
+            acc + abs(first - second)
+        }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
